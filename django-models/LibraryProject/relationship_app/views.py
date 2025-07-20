25403b7082fddc5fc0f773d60ae83c0d2089f7f7
Ohazulike Stanley <sohazulike@gmail.com>
2025-07-20 16:23:04 +1000

    updated relationsip-app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+
+
+# Class-based view for library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
