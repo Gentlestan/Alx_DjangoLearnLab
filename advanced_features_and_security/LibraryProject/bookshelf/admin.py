163e6749d3b644684f0d0d03a28bb184781bab7d
Ohazulike Stanley <sohazulike@gmail.com>
2026-01-28 05:54:04 +0100

    cretated admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,11 @@
-
-# Register your models here.
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Display these fields in the admin list view
-    search_fields = ('title', 'author')  # Add a search box for title and author
-    list_filter = ('publication_year',)  # Add a filter sidebar for publication year
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
