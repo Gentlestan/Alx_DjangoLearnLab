74bcb33df8bd5e9ed5ca0b3595f9f5fa45a1957e
Ohazulike Stanley <sohazulike@gmail.com>
2026-01-28 05:51:14 +0100

    cretated custom user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError(_("The username must be set"))
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -11,7 +36,7 @@ class CustomUser(AbstractUser):
         blank=True
     )
 
-    # required to avoid clashes if another CustomUser exists
+    # prevent reverse accessor clashes
     groups = models.ManyToManyField(
         "auth.Group",
         related_name="bookshelf_customuser_groups",
@@ -22,6 +47,8 @@ class CustomUser(AbstractUser):
         related_name="bookshelf_customuser_permissions",
         blank=True,
     )
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
