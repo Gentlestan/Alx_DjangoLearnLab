d5810ae19ad1cbf531a53a14c012721811c458be
Ohazulike Stanley <sohazulike@gmail.com>
2026-01-28 05:49:28 +0100

    cretated updared user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,19 +1,43 @@
 from django.db import models
-from django.conf import settings  # ✅ use this to reference the user model
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(
+        upload_to="profile_photos/",
+        null=True,
+        blank=True
+    )
+
+    # required to avoid clashes if another CustomUser exists
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="bookshelf_customuser_groups",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="bookshelf_customuser_permissions",
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.username
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
-    # Reference the custom user as a foreign key
-owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL,  # points to CustomUser
-        on_delete=models.CASCADE,   # if user is deleted, delete their books
-        related_name="bookshelf_books",  # optional: lets you access user.books
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
         null=True,
         blank=True
     )
 
-def __str__(self):
+    def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
