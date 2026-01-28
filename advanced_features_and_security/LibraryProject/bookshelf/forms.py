2db5da0ca0b9c80c267709c845bc78bd2706d152
Ohazulike Stanley <sohazulike@gmail.com>
2026-01-28 07:06:23 +0100

    added security

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Book
 
-# ALX checker requires ExampleForm
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author", "publication_year"]
+
 class ExampleForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm  # you need to create this form
+from .forms import BookForm, ExampleForm  # you need to create this form
 
 
 @permission_required("bookshelf.can_view", raise_exception=True)
