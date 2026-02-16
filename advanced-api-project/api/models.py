af135f0e260827a070cab737cbcc5bfb3112088c
Ohazulike Stanley <sohazulike@gmail.com>
2026-02-16 07:14:19 +0100

    changed dir name

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Author(models.Model):
+    """
+    Represents a book author.
+    One Author can have multiple Books.
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Represents a Book written by an Author.
+    Each Book belongs to one Author.
+    """
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author,
+        related_name='books',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
