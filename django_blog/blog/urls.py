2b50de4cd330a368e6472bafc388bf9bdc2c7b2b
Ohazulike Stanley <sohazulike@gmail.com>
2026-02-18 17:11:07 +0100

    updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -32,7 +32,9 @@ urlpatterns = [
     # -------------------------
     # Tag Filtering
     # -------------------------
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
+      # ✅ Tag filtering using slug
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+
 
     # -------------------------
     # Search
