912154068592e1d72a6210b6877ea2454581076c
Ohazulike Stanley <sohazulike@gmail.com>
2026-02-18 09:06:54 +0100

    updated urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     # -------------------------
     # Comment CRUD (class-based)
     # -------------------------
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
