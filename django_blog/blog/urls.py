178487c110fb9267cb3181b2f0ecc65c915d0eba
Ohazulike Stanley <sohazulike@gmail.com>
2026-02-18 08:28:35 +0100

    added post

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,23 +1,22 @@
 # blog/urls.py
 from django.urls import path
-from . import views_auth
-from . import views_posts
+from . import views  # <- now all views are in this file
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Home
-    path('', views_auth.home, name='home'),
+    path('', views.home, name='home'),
 
     # Authentication URLs
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
-    path('register/', views_auth.register, name='register'),
-    path('profile/', views_auth.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 
     # Blog Post CRUD URLs
-    path('posts/', views_posts.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views_posts.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views_posts.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views_posts.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views_posts.PostDeleteView.as_view(), name='post-delete'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,53 @@
-# blog/views_posts.py
+# blog/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from .forms import PostForm
+from .forms import RegisterForm, ProfileUpdateForm, PostForm
+
+
+# -------------------------
+# Authentication / Profile
+# -------------------------
+
+# Home view
+def home(request):
+    return render(request, 'blog/index.html')
+
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+# Profile view
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
+
+
+# -------------------------
+# Blog Post CRUD Views
+# -------------------------
 
 # List all posts (public)
 class PostListView(ListView):
